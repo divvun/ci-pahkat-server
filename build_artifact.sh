2c87621464d7aec3ccc1af0d2640aceb9b309517
Eike Siewertsen <eike@technocreatives.com>
2019-11-14 13:38:12 +0100

    fix ci

--- build_artifact.sh
+++ build_artifact.sh
@@ -7,11 +7,9 @@ rm -rf $DIST && mkdir $DIST
 
 cd $ROOT
 
-echo "copying configs"
-cp docker-compose.yml $DIST
-
-# git clone --single-branch --branch develop --depth=1 https://github.com/divvun/divvun-api.git || (cd divvun-api && git pull && cd ..)
-docker build -t divvun/pahkat-server ../pahkat
+git clone --single-branch --branch develop --depth=1 https://github.com/divvun/pahkat.git || (cd pahkat && git pull && cd ..)
+docker build -t divvun/pahkat-server pahkat
+# docker build -t divvun/pahkat-server ../pahkat
 docker save divvun/pahkat-server | gzip > $DIST/pahkat-server.tar.gz
 
 pushd $DIST
